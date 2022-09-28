22a9b9488c2f9894fe0cf28cc211d01442b00fd4
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-09-28 14:00:41 +0100

    Setup React Router and add route for Home view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Navbar from "./components/Navbar/Navbar";
+import Home from "./views/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
