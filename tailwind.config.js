36cdc5cbaa3b08d67ff5152b25b2392db7dfdea2
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-10-01 11:20:22 +0100

    Change font family for entire application

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "noto-sans": ['"Noto Sans"', "sans-serif"],
+      },
+      backgroundImage: {
+        art: "url(/src/media/images/art.webp)",
+      },
+    },
   },
   plugins: [],
 };
