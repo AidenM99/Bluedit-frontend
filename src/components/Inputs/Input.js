719c3fb0dd265781e25182ced6dbceebf9721233
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-10-01 11:24:26 +0100

    Add additional props to Input component

--- src/components/Inputs/Input.js
+++ src/components/Inputs/Input.js
@@ -1,9 +1,24 @@
-const Input = ({ type, value, name, placeholder, onChange, styles }) => {
+const Input = ({
+  type,
+  value,
+  name,
+  id,
+  placeholder,
+  required,
+  minLength,
+  maxLength,
+  onChange,
+  styles,
+}) => {
   return (
     <input
       type={type}
       value={value}
       name={name}
+      id={id}
+      required={true}
+      minLength={minLength}
+      maxLength={maxLength}
       placeholder={placeholder}
       onChange={onChange}
       className={styles}
